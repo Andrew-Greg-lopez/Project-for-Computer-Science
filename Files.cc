4c9451c660aa9adeeedd40618d4970eb9f554cbd
Andrew-Greg-lopez <andrewlopez472@yahoo.com>
2025-01-02 23:05:32 -0600

    Create Files.cc

---
+++ Files.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string filename = "Andrew'secretsuprise.txt";
+    ifstream inputfile;
+    int number, count = 0, sum = 0;
+
+  
+    inputfile.open(filename);
+
+   
+    if (inputfile.is_open()) {   
+        cout << "File did open which is great!" << endl;
+    } else {
+        cout << "I'm sorry you gonna have to try that again" << endl;
+        return 1; 
+    }
+
+    
+    inputfile.close();
+
+    return 0; 
+}
